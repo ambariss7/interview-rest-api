aefd75410ff69b8ae97c741990d27774ed0006f1
ambariss7 <al.ambari79@gmail.com>
2021-04-09 15:45:10 +0700

    First Commit

---
+++ database/migrations/2021_04_09_072808_create_positions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('status', 255);
+            $table->string('position', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
